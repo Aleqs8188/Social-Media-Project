6c3dbb87a57aa6c51599d15c872122b20267c093
Лёха Свик <alexbulgak70@gmail.com>
2022-08-29 19:30:44 +0200

    release v3

--- website/src/components/Profile/MyPosts/MyPosts.jsx
+++ website/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,11 +1,9 @@
 import Post from "./Post/Post";
 import s from './MyPosts.module.css';
-import React, { useState } from 'react';
-import useForceUpdate from 'use-force-update';
+import React from 'react';
 import {addNewPostActionCreator, addPostActionCreator, clearNewPostActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
-    const forceUpdate = useForceUpdate();
     let postsElements = props.posts .map(p => <Post message={p.message} likesCount={p.likesCount}/>)
     let newPostText = props.newPostText;
 
@@ -22,7 +20,6 @@ const MyPosts = (props) => {
     let clickHandler = () => {
         addPost();
         clearInput();
-        forceUpdate();
     }
     return (
         <div className={s.postsBlock}>
